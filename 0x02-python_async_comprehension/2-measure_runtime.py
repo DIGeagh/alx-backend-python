d5dbb0b170d86f22881d85265b63c06a54c625a7
DIGeagh <darlingtongavi@hotmail.com>
2024-10-15 18:24:41 +0000

    async

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 """
-A module for measuring the time it takes to run a given number of
-asynchronous tasks concurrently.
+This module provides an asynchronous function that measures the runtime
+of running 4 instances of the async_comprehension function from the
+'1-async_comprehension' module
 """
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines').wait_n
+from importlib import import_module as using
+
+async_comprehension = using('1-async_comprehension').async_comprehension
 
 
-def measure_time(n: int, max_delay: int) -> float:
+async def measure_runtime() -> float:
     """
-    Measure the time it takes to run `wait_n` with `n` coroutines
-    that wait for random amounts of time up to `max_delay` seconds.
-    Returns the average time taken per coroutine.
+    Asynchronous function that measures and returns the runtime of
+    running 4 instances of the async_comprehension function.
+
+    Returns:
+        float: The runtime of the async_comprehension function in seconds.
     """
     start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    return (time.time() - start_time) / n
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
